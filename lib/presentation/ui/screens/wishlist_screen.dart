c428b74dc7b0bf819e78b99741cdcf0a19a34a9d
shuvosonjoy <shuvosdg123@gmail.com>
2024-01-28 03:31:07 +0600

    wishlist

--- lib/presentation/ui/screens/wishlist_screen.dart
+++ lib/presentation/ui/screens/wishlist_screen.dart
@@ -1,4 +1,7 @@
+import 'package:craftybay/presentation/state_holders/main_bottom_nav_controller.dart';
+import 'package:craftybay/presentation/ui/widgets/product_card_item.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class WishListScreen extends StatefulWidget {
   const WishListScreen({super.key});
@@ -10,6 +13,42 @@ class WishListScreen extends StatefulWidget {
 class _WishListScreenState extends State<WishListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (value){
+        Get.find<MainBottomNavController>().backToHome();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Get.find<MainBottomNavController>().backToHome();
+              }),
+          title: Text(
+            'Wish List',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.builder(
+              itemCount: 100,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 4,
+                  childAspectRatio: 0.90),
+              itemBuilder: (context, index) {
+                return FittedBox(
+                  child: ProductCardItem(),
+                );
+              }),
+        ),
+      ),
+    );
   }
-}
+  }
+
