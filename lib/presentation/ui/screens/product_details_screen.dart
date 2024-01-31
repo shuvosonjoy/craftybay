c3a14723a20fa1a7239e3854b4c1bce73bad339c
shuvosonjoy <shuvosdg123@gmail.com>
2024-02-01 02:02:54 +0600

    color and size selector

--- lib/presentation/ui/screens/product_details_screen.dart
+++ lib/presentation/ui/screens/product_details_screen.dart
@@ -12,6 +12,17 @@ class ProductDetailsScreen extends StatefulWidget {
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   ValueNotifier<int> noOfItems = ValueNotifier(1);
+  Color _selectedColor = Colors.grey;
+  List<Color> colors = [
+    Colors.grey,
+    Colors.red,
+    Colors.black,
+    Colors.amber,
+    Colors.green
+  ];
+  List<String>sizes = [
+    'L','M','XL','XXL','XXXL'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +39,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                   Padding(
                     padding: const EdgeInsets.all(16),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
                           children: [
@@ -58,50 +70,86 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                             ),
                           ],
                         ),
-                        Row(children: [
-                          Wrap(
-                              crossAxisAlignment: WrapCrossAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.star,
-                                  size: 14,
-                                  color: Colors.amber,
-                                ),
-                                Text(
-                                  '4.4',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.black54),
-                                ),
-                              ]),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            'Reviews',
-                            style: TextStyle(
-                              color: AppColors.primaryColor,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Wrap(
+                                crossAxisAlignment: WrapCrossAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    size: 16,
+                                    color: Colors.amber,
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    '4.4',
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.black54),
+                                  ),
+                                ]),
+                            SizedBox(
+                              width: 8,
                             ),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Card(
-                            color: AppColors.primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(2.0),
-                              child: Icon(
-                                Icons.favorite_border_rounded,
-                                size: 10,
-                                color: Colors.white,
+                            Text(
+                              'Reviews',
+                              style: TextStyle(
+                                color: AppColors.primaryColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
-                          )
-                        ]),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Card(
+                              color: AppColors.primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Icon(
+                                  Icons.favorite_border_rounded,
+                                  size: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          'Colors',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        ColorSelector(colors: colors, onChange:(selectedColor){
+                          _selectedColor=selectedColor;
+                        }),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          'Size',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        SizeSelector(sizes: sizes, onChange: (s){})
+
                       ],
                     ),
                   )
@@ -156,5 +204,112 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
         ],
       ),
     );
+  }
+}
+class ColorSelector extends StatefulWidget {
+  const ColorSelector({super.key, required this.colors, required this.onChange});
+  final List<Color> colors;
+  final Function(Color) onChange;
+
+  @override
+  State<ColorSelector> createState() => _ColorSelectorState();
+}
+
+class _ColorSelectorState extends State<ColorSelector> {
+  late Color _selectedColor ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _selectedColor=widget.colors.first;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+        children: widget.colors
+            .map((c) => Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(15),
+            onTap: () {
+              _selectedColor = c;
+              widget.onChange(c);
+              if (mounted) {
+                setState(() {});
+              }
+            },
+            child: CircleAvatar(
+              backgroundColor: c,
+              radius: 15,
+              child: _selectedColor == c
+                  ? Icon(
+                Icons.done,
+                color: Colors.white,
+              )
+                  : null,
+            ),
+          ),
+        ))
+            .toList());
+  }
+}
+
+
+
+
+class SizeSelector extends StatefulWidget {
+  const SizeSelector({super.key, required this.sizes, required this.onChange});
+  final List<String> sizes;
+  final Function(String) onChange;
+
+  @override
+  State<SizeSelector> createState() => _SizeSelectorState();
+}
+
+class _SizeSelectorState extends State<SizeSelector> {
+  late String _selectedSize ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _selectedSize=widget.sizes.first;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+        children: widget.sizes
+            .map((c) => Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(15),
+            onTap: () {
+              _selectedSize = c;
+              widget.onChange(c);
+              if (mounted) {
+                setState(() {});
+              }
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 12,vertical: 8),
+              margin: EdgeInsets.only(right: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: Colors.grey),
+                color: _selectedSize ==c ? AppColors.primaryColor : null,
+              ),
+              child: Text(c,style: TextStyle(
+                color: _selectedSize == c ? Colors.white : Colors.grey
+              ),)
+
+            ),
+          ),
+        ))
+            .toList());
   }
 }
